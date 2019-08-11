d78733afde0f77bc8db6717463cf6df484bb060e
YuichiroSS <40968862+YuichiroSS@users.noreply.github.com>
2019-08-11 22:13:48 +0900

    Add files via upload

--- hackU/src/sqlwrite.py
+++ hackU/src/sqlwrite.py
@@ -1,22 +1,25 @@
 import csv
 import sqlite3
 
+# MENU.dbを作成する
+dbname = 'MENU.db'
+
 # DBを表すConnectionオブジェクトをRAM上に作成して、DBに接続する。
-conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect(dbname)
 # DB上の処理対象の行を指し示すためのCursorオブジェクトを作成する
 cur = conn.cursor()
 
 # ごはんという名前のtableを作成する
 cur.execute("""CREATE TABLE ごはん
-(メニュー名 CHAR(32) NOT NULL,
-カロリー INTEGER NOT NULL,
-タンパク質 DOUBLE NOT NULL,
-脂質 DOUBLE NOT NULL,
-炭水化物 DOUBLE NOT NULL,
-塩分 DOUBLE NOT NULL,
-赤 DOUBLE NOT NULL,
-緑 DOUBLE NOT NULL,
-黄 DOUBLE NOT NULL);""")
+(メニュー名 STRING,
+カロリー INTEGER,
+タンパク質 DOUBLE,
+脂質 DOUBLE,
+炭水化物 DOUBLE,
+塩分 DOUBLE,
+赤 DOUBLE,
+緑 DOUBLE,
+黄 DOUBLE);""")
 
 # csvファイルを開く
 with open('CSV\ごはん.csv', 'rt', encoding="utf-8") as f:
@@ -30,4 +33,5 @@ with open('CSV\ごはん.csv', 'rt', encoding="utf-8") as f:
 conn.commit()
 
 #DBとの接続を閉じる
+cur.close()
 conn.close()
