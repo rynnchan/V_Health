ffef14eb769fa0fb309c4e02927d3b32b447fc40
rynnchan <ogw-rnt@outlook.jp>
2019-08-12 23:34:24 +0900

    MainでColorcheckクラスを実行できるようにMainを変更

--- hackU/src/Main.java
+++ hackU/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     
     /** main **/
     public static void main(String[] args) {
-    
-        System.out.println("Hello World !");
+        double[] colors1 = {4.5,5,4};
+        double[] colors2 = {6,5,10};
+        Colorcheck colorcheck1 = new Colorcheck(colors1);
+        Colorcheck colorcheck2 = new Colorcheck(colors2);
+        System.out.println(colorcheck1.minColor());
+        System.out.println(colorcheck2.minColor());
+        System.out.println(colorcheck1.minColor());
     }
 }
