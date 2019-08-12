6ebcb66289de7fa740066c89502f42d492400c18
rynnchan <ogw-rnt@outlook.jp>
2019-08-12 23:33:00 +0900

    ColorCheck.javaをクラスらしくした

--- hackU/src/Colorcheck.java
+++ hackU/src/Colorcheck.java
@@ -1,27 +1,36 @@
 public class Colorcheck {
-    public static int cnt = 0;
+    public static final int RED = 0;
+    public static final int GREEN = 1;
+    public static final int YELLOW = 2;
+    private static int cnt = 0;
+    private static double[][] list = new double[3][21];
+
+    private double[] colors = new double[3];
+
+    Colorcheck(double[] colors){
+        this.colors = colors;
+    }
 
     // 各群の点数を要素とする配列を受け取り、過去21回分（1週間分）の各群の点数の和を計算
     // そして、その和から最も点数の少ない群をintで返す関数
-    // 0：赤、1：緑、2：黄
-    public static int colorcheck(double[] colors) {
-        double[][] list = new double[3][21];
+    public int minColor() {
+        double[] colors = getColors();
         double[] sum = new double[3];
-        int color = 0;
+        int color = RED;
 
         // 受け取った各群の点数を配列に格納し、これまでの点数和を算出
         for (int i = 0; i < 3; i++) {
             list[i][cnt] = colors[i];
-            for (int j = 0; j < cnt; j++) {
+            for (int j = 0; j < 21; j++) {
                 sum[i] += list[i][j];
             }
         }
         
         // 最も点数の少ない群の探索
         if (sum[0] > sum[1] && sum[2] > sum[1])
-            color = 1;
+            color = GREEN;
         else if (sum[0] > sum[2] && sum[1] > sum[2])
-            color = 2;
+            color = YELLOW;
 
         // カウンターの調整
         // 21回記録したら、カウンターを0に戻す
@@ -30,9 +39,18 @@ public class Colorcheck {
             cnt = 0;
         
         return color;
-        }
-
-    public static void main(String[] args) {
-
     }
+
+    public double[] getColors(){
+        return this.colors;
+    }
+    public static double[][] getList(){
+        return list;
+    }
+
+    public void setColors(double[] colors){
+        this.colors = colors;
+    }
+
+
 }
