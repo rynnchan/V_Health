8a7cb60207a57d779be765904e3df548a8eead66
YuichiroSS <40968862+YuichiroSS@users.noreply.github.com>
2019-08-14 15:23:42 +0900

    Add files via upload

--- hackU/src/Colorpoint.java
+++ hackU/src/Colorpoint.java
@@ -1,9 +1,14 @@
 public class Colorpoint {
     public static final double p = 0.65; // ポアソン分布的なp
+    public static final int MALE_POINT = 16;
+    public static final int FEMALE_POINT = 11;
+    public static final int MALE = 0; // 男性を0とし
+    public static final int FEMALE = 1;  // 女性を1とする
+
     // 3群の点数を用いて、栄養バランスによるパラメータ（[0,1]）を算出する
     // 各群の点数が格納された配列を受け取る
     // color[0]：赤、color[1]：緑、color[2]：黄
-    public double[] colorpoint(double[] color) {
+    public double[] colorpoint(double[] color, int sex) {
         double[] colorpoint = new double[2];  // colorpoint[0]：栄養バランス 悪い、colorpoint[1]：栄養バランス 良い
         double red = color[0];
         double green = color[1];
@@ -14,7 +19,11 @@ public class Colorpoint {
         colorpoint[0] = 1 - (first / 9);
             if (colorpoint[0] < 0) 
                 colorpoint[0] = Math.abs(colorpoint[0]);
-        colorpoint[1] = first * p + yellow * (1 - p);  // 増減可能得点を含めて栄養バランスの良いパラメータを設定
+        
+        if (sex == MALE)
+            colorpoint[1] = colorpoint[0] * p + (1 - (yellow / MALE_POINT))  * (1 - p);  // 増減可能得点を含めて栄養バランスの良いパラメータを設定
+        else
+            colorpoint[1] = colorpoint[0] * p + (1 - (yellow / FEMALE_POINT)) * (1 - p);
 
         return colorpoint;
     }
