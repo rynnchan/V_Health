defb6568eb3ef4ea73e487afda42dc292c88af63
rynnchan <ogw334@gmail.com>
2019-08-19 13:32:26 +0900

    averageを追加したことによるその他ファイルの変更(とりあえず最終)

--- hackU/src/Main.java
+++ hackU/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 public class Main {
     
     public static void main(String[] args) {
@@ -23,20 +24,49 @@ public class Main {
         // これより上の変数は実際にはフロントエンド側からJSONで受け取る
 
         Read_json aj = new Read_json("hackU/json/average.json");
-        double ave_calorie=Read_json.read_ave_calorie(aj.root);
-        double ave_protein=Read_json.read_ave_protein(aj.root);
-        double ave_lipid=Read_json.read_ave_lipid(aj.root);
-        double ave_carbohydrate=Read_json.read_ave_carbohydrate(aj.root);
-        double ave_salt=Read_json.read_ave_salt(aj.root);
-        double ave_red=Read_json.read_ave_red(aj.root);
-        double ave_green=Read_json.read_ave_green(aj.root);
-        double ave_yellow=Read_json.read_ave_yellow(aj.root);
-        double[] ave_color={ave_red,ave_green,ave_yellow};
         int start=Read_json.read_start(aj.root);
+        int today_y=Read_json.read_today(aj.root);
+
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(start/10000, (start%10000)/100-1, start%100);
+        long diffTime = cal1.getTimeInMillis() - cal2.getTimeInMillis();
+        double diffDayMillis = 1000 * 60 * 60 * 24;
+        int n = (int) (Math.ceil(((double)diffTime / diffDayMillis)));
+
+        double today_calorie=calorie;
+        double today_protein=protein;
+        double today_lipid=lipid;
+        double today_carbohydrate=carbohydrate;
+        double today_salt=salt;
+        double today_red=color[0];
+        double today_green=color[1];
+        double today_yellow=color[2];
+        int today = cal1.get(Calendar.YEAR)*10000+(cal1.get(Calendar.MONTH)+1)*100+cal1.get(Calendar.DAY_OF_MONTH);
+        if(today==today_y){
+            today_calorie+=Read_json.read_today_calorie(aj.root);
+            today_protein+=Read_json.read_today_protein(aj.root);
+            today_lipid+=Read_json.read_today_lipid(aj.root);
+            today_carbohydrate+=Read_json.read_today_carbohydrate(aj.root);
+            today_salt+=Read_json.read_today_salt(aj.root);
+            today_red+=Read_json.read_today_red(aj.root);
+            today_green+=Read_json.read_today_green(aj.root);
+            today_yellow+=Read_json.read_today_yellow(aj.root);
+        }
+
+        double ave_calorie=Read_json.read_ave_calorie(aj.root)*(n-1/n)+today_calorie/n;
+        double ave_protein=Read_json.read_ave_protein(aj.root)*(n-1/n)+today_protein/n;
+        double ave_lipid=Read_json.read_ave_lipid(aj.root)*(n-1/n)+today_lipid/n;
+        double ave_carbohydrate=Read_json.read_ave_carbohydrate(aj.root)*(n-1/n)+today_carbohydrate/n;
+        double ave_salt=Read_json.read_ave_salt(aj.root)*(n-1/n)+today_salt/n;
+        double ave_red=Read_json.read_ave_red(aj.root)*(n-1/n)+today_red/n;
+        double ave_green=Read_json.read_ave_green(aj.root)*(n-1/n)+today_green/n;
+        double ave_yellow=Read_json.read_ave_yellow(aj.root)*(n-1/n)+today_yellow/n;
+        double[] ave_color={ave_red,ave_green,ave_yellow};
+
         try {
-            String sa = Send_average.send_average(ave_calorie,ave_protein,ave_lipid,ave_carbohydrate,ave_salt,ave_red,ave_green,ave_yellow,start);
-            System.out.println(sa);
-            Send_front.write_json(sa);
+            String sa = Send_average.send_average(ave_calorie,ave_protein,ave_lipid,ave_carbohydrate,ave_salt,ave_red,ave_green,ave_yellow,start,today,today_calorie,today_protein,today_lipid,today_carbohydrate,today_salt,today_red,today_green,today_yellow);
+            Send_average.write_json(sa);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,7 +82,6 @@ public class Main {
 
         try {
             String su = Send_unity.send_unity(shape[1], shape[0], sleepy, balance[1], balance[0]);
-            System.out.println(su);
             Send_unity.write_json(su);
         } catch (Exception e) {
             e.printStackTrace();
@@ -70,7 +99,6 @@ public class Main {
 
         try {
             String sf = Send_front.send_front(menu, ave, sleep_hour);
-            System.out.println(sf);
             Send_front.write_json(sf);
         } catch (Exception e) {
             e.printStackTrace();
